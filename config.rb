d47062ae63a2806079592b460cd131495a73cab2
Gianluca Monti <gm3871@gmail.com>
2017-09-08 19:15:40 +0200

    compressione css

--- config.rb
+++ config.rb
@@ -19,11 +19,12 @@ javascripts_dir = "javascripts"
 
 preferred_syntax = :scss
 
-css_dir = "stylesheets" # by Compass.app
-sass_dir = "sass" # by Compass.app
-images_dir = "images" # by Compass.app
-output_style = :expanded # by Compass.app
-relative_assets = true # by Compass.app
-line_comments = false # by Compass.app
-sass_options = {:debug_info=>false} # by Compass.app
-sourcemap = true # by Compass.app
+
+css_dir = "stylesheets" # by Compass.app 
+sass_dir = "sass" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = true # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = true # by Compass.app 
